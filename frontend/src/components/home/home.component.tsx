4650c64f28dea31e408916524d815c46a56dc823
RachelCodes247 <dsouzarachel2004@gmail.com>
2026-06-01 14:26:31 +0530

    feat: add back-to-top button on homepage for better navigation

--- frontend/src/components/home/home.component.tsx
+++ frontend/src/components/home/home.component.tsx
@@ -10,6 +10,7 @@ import WriterFeedbackComponent from "./writer_feedback/writer_feedback.component
 import StartWritingComponent from "./start_writing/start_writing.component";
 import PersonalizedRecommendationsComponent from "./personalized_recommendations/personalized_recommendations.component";
 import { isLoggedIn } from "../../services/auth.service";
+import BackToTop from "../back_home/back_to_top.component";
 
 const HomeComponent = () => {
   const isLogin = isLoggedIn();
@@ -17,12 +18,13 @@ const HomeComponent = () => {
     <>
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pt-10 pb-10">
         <div className="grid grid-cols-12 items-start gap-8 mb-10">
-          <div className="col-span-12 lg:col-span-8 min-w-0">
+          <div className="col-span-12 lg:col-span-8 min-w-0 flex flex-col gap-8">
             <FeatureComponent />
             <LatestPostsComponent />
           </div>
           <div className="col-span-12 lg:col-span-4 min-w-0">
-            <div className="sticky top-24 space-y-6">
+            {/* Added flex flex-col gap-6 to explicitly enforce vertical layout separation */}
+            <div className="sticky top-24 flex flex-col gap-6">
               {isLogin && <FeatureProfileComponent />}
               {isLogin && <PersonalizedRecommendationsComponent />}
               <TrendingTopicComponent />
@@ -36,6 +38,7 @@ const HomeComponent = () => {
       <WriterFeedbackComponent />
       <PricingComponent />
       <StartWritingComponent />
+      <BackToTop />
     </>
   );
 };
